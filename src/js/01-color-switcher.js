474f03523c7cf9a603181fcb717dac8fe9c1d58e
valeeriiiee <obleavalerie@gmail.com>
2024-03-06 22:18:41 +0800

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+const body = document.querySelector('body')
+const startBtn = document.querySelector('button[data-start]')
+const stopBtn = document.querySelector('button[data-stop]')
+let colorChangeInterval = null;
+stopBtn.disabled = true;
+
+function startColorChange() {
+    colorChangeInterval = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor()
+        startBtn.disabled = true;
+        stopBtn.disabled = false;
+    }, 1000)
+}
+
+console.log(startBtn);
+
+function stopColorChange() {
+    clearInterval(colorChangeInterval);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+}
+
+startBtn.addEventListener('click', startColorChange);
+stopBtn.addEventListener('click', stopColorChange);
